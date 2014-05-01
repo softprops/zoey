1b4e190c09470cf0092459d13286516af464f70a
softprops <d.tangren@gmail.com>
2014-05-01 08:36:32 -0400

    use client port

--- src/test/scala/ZkClientSpec.scala
+++ src/test/scala/ZkClientSpec.scala
@@ -8,18 +8,19 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import scala.util.control.NonFatal
 
 class ZkClientSpec extends FunSpec with BeforeAndAfterAll with ZkServer {
-  val host = new InetSocketAddress(0)
-  val svr = server(host)
+  lazy val host = new InetSocketAddress(2181)
+  lazy val svr = server(host)
 
   describe("ZkClient") {
     it ("should work") {
-      val zk = ZkClient(host.toString)
+      val zk = ZkClient(s"${host.getHostName}:${svr.getClientPort}")
       val future = zk.aclOpenUnsafe.ephemeral("/test").create()
       future.foreach(println)
       future.onFailure {
         case NonFatal(e) => e.printStackTrace
       }
       Await.ready(future, Duration.Inf)
+      zk.close()
     }
   }
 
