52d05e478523cbfa3c36c3899b5ea4b3c8407a1d
softprops <d.tangren@gmail.com>
2014-05-01 08:49:06 -0400

    assert something

--- src/test/scala/ZkClientSpec.scala
+++ src/test/scala/ZkClientSpec.scala
@@ -8,18 +8,18 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import scala.util.control.NonFatal
 
 class ZkClientSpec extends FunSpec with BeforeAndAfterAll with ZkServer {
-  lazy val host = new InetSocketAddress(2181)
+  lazy val host = new InetSocketAddress(0)
   lazy val svr = server(host)
 
   describe("ZkClient") {
     it ("should work") {
       val zk = ZkClient(s"${host.getHostName}:${svr.getClientPort}")
-      val future = zk.aclOpenUnsafe.ephemeral("/test").create()
-      future.foreach(println)
+      val path = "/test"
+      val future = zk.aclOpenUnsafe.ephemeral(path).create()
       future.onFailure {
         case NonFatal(e) => e.printStackTrace
       }
-      Await.ready(future, Duration.Inf)
+      assert(Await.result(future, Duration.Inf).path === path)
       zk.close()
     }
   }
