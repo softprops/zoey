99221ec254be67195e7f1b4cdd5e7743e7e017da
softprops <d.tangren@gmail.com>
2014-05-02 00:58:22 -0400

    reconnect test

--- src/test/scala/ConnectionIssuesSpec.scala
+++ src/test/scala/ConnectionIssuesSpec.scala
@@ -4,13 +4,21 @@ import org.scalatest.{ BeforeAndAfterAll, FunSpec }
 import scala.concurrent.duration._
 import scala.concurrent.Await
 import scala.concurrent.ExecutionContext.Implicits.global
+import retry.Defaults.timer
 import scala.util.control.NonFatal
+import java.net.InetSocketAddress
 
 class ConnectionIssuesSpec extends FunSpec with ZkServer {
-  
   describe("ConnectionIssues") {
     it ("should reconnect") {
-
+      val addr = new InetSocketAddress(2181)
+      val cli = ZkClient(s"${addr.getHostName}:${addr.getPort}", Some(2.seconds)).retried()
+      println("applying future")
+      val future = cli()
+      future.onFailure {
+        case NonFatal(e) => println(s"failing $e")
+      }
+      Await.ready(future, Duration.Inf)
     }
   }
 }
