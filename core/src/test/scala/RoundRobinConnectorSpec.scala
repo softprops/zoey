d91109a1f2606786722c3cd183cb8ad2a2635ea2
softprops <d.tangren@gmail.com>
2014-06-03 08:39:24 -0400

    close the client as part of the spec

--- core/src/test/scala/RoundRobinConnectorSpec.scala
+++ core/src/test/scala/RoundRobinConnectorSpec.scala
@@ -11,14 +11,14 @@ import java.net.InetSocketAddress
 class RoundRobinConnectorSpec extends FunSpec
   with BeforeAndAfterAll with testing.ZkServer {
   val addrs = (2181 to 2183).map(new InetSocketAddress(_))
+  val strAddrs = addrs.map(addr => s"${addr.getHostName}:${addr.getPort}").toList
   lazy val svr = server(addrs.last)
+  lazy val cli = ZkClient.roundRobin(
+    strAddrs, Some(2.seconds)).retried(max = 3)
 
   describe("RoundRobinConnector") {
     it ("should retry failed operations on each server provided") {
-      val strAddrs = addrs.map(addr => s"${addr.getHostName}:${addr.getPort}").toList
       svr // trigger svr startup
-      val cli = ZkClient.roundRobin(
-        strAddrs, Some(2.seconds)).retried(max = 3)
       val future = cli()
       future.onFailure {
         case NonFatal(e) => println(s"failing $e")
@@ -31,6 +31,7 @@ class RoundRobinConnectorSpec extends FunSpec
   }
 
   override def afterAll() {
-    svr.shutdown()
+    cli.close()
+    svr.close()
   }
 }
