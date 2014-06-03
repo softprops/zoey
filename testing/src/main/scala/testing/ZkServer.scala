53994049098117b3f101d2a729c9407e4014c853
softprops <d.tangren@gmail.com>
2014-06-03 08:39:48 -0400

    closeable

--- testing/src/main/scala/testing/ZkServer.scala
+++ testing/src/main/scala/testing/ZkServer.scala
@@ -1,9 +1,9 @@
 package zoey.testing
 
-import java.io.File
 import org.apache.zookeeper.server.{
   NIOServerCnxnFactory, ServerCnxnFactory, ZooKeeperServer
 }
+import java.io.Closeable
 import java.util.UUID
 import java.net.InetSocketAddress
 import scala.concurrent.Future
@@ -12,13 +12,11 @@ import scala.concurrent.ExecutionContext.Implicits.global
 /** provides access to in memory standalone zk servers on the fly */
 trait ZkServer {
 
-  trait Server {
+  trait Server extends Closeable {
     /** @return true, if server is currently running */
     def isRunning: Boolean
-    /** shuts the a server down */
-    def shutdown(): Unit
     /** address a zk client can resolve server from */
-    def clientAddr: String
+    def connectStr: String
     /** persists state */
     def save(): Unit
   }
@@ -27,17 +25,16 @@ trait ZkServer {
     host: InetSocketAddress = new InetSocketAddress(Port.random),
     maxConnections: Int = 100,
     tickTime: Int = ZooKeeperServer.DEFAULT_TICK_TIME): Server = {
-    val path = new File(System.getProperty("java.io.tmpdir"), "zk-" + UUID.randomUUID())
-    path.deleteOnExit()
+    val path = Files.randomTemp
     val server = new ZooKeeperServer(path, path, tickTime)
     val fact = ServerCnxnFactory.createFactory()
     fact.configure(host, maxConnections)
     fact.startup(server)
     new Server {
       def isRunning = server.isRunning
-      def clientAddr = s"${host.getHostName}:${server.getClientPort}"
+      def connectStr = s"${host.getHostName}:${server.getClientPort}"
       def save() = server.takeSnapshot()
-      def shutdown() = {
+      def close() = {
         if (!isRunning) {
           server.shutdown()
         }
