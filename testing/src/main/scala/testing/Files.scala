1851f8223da9e1bdff43f148617607912db1c7a5
softprops <d.tangren@gmail.com>
2014-06-03 08:36:26 -0400

    file testing util for creating ephemeral random data dirs

---
+++ testing/src/main/scala/testing/Files.scala
@@ -0,0 +1,30 @@
+package zoey.testing
+
+import java.util.Random
+import java.lang.{ Integer => JInt }
+import java.io.File
+
+object Files {
+  private lazy val random = new Random
+  /** creates a new random temporary dir */
+  def randomTemp = {
+    val f = new File(
+      System.getProperty("java.io.tmpdir"),
+      "zk-" + JInt.toHexString(random.nextInt))
+    f.mkdir()
+    Runtime.getRuntime.addShutdownHook(new Thread {
+      override def run = if (f.exists) {
+        def delete(file: File): Unit =
+          file match {
+            case dir if (dir.isDirectory) =>
+              dir.listFiles.foreach(delete)
+              dir.delete()
+            case plain =>
+              plain.delete()
+          }
+        delete(f)
+      }
+    })
+    f
+  }
+}
