0d8b157ac1907542876daaf145476c30af203025
softprops <d.tangren@gmail.com>
2014-10-19 19:49:27 -0400

    release version

--- project/Common.scala
+++
@@ -1,8 +0,0 @@
-import sbt._, sbt.Keys._
-
-object Common {
-  def module(mod: String) =
-    Project(mod, file(mod), 
-            settings = Defaults.defaultSettings ++ Seq(
-              name := s"zoey-$mod"))
-}
