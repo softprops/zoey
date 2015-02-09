f7371020788ca72cbe5018925f3b33a7f313fd15
softprops <d.tangren@gmail.com>
2015-02-08 22:23:49 -0500

    friendlier path appending

--- zoey-core/src/main/scala/Paths.scala
+++ zoey-core/src/main/scala/Paths.scala
@@ -4,18 +4,26 @@ trait Paths {
 
   def path: String
 
-  lazy val name: String = path.lastIndexOf('/') match {
-    case i if i == -1 || i == path.length - 1 => ""
-    case i => path.substring(i + 1)
-  }
+  lazy val name: String =
+    path.lastIndexOf('/') match {
+      case i if i == -1 || i == path.length - 1 => ""
+      case i => path.substring(i + 1)
+    }
 
-  lazy val parentPath: String = path.lastIndexOf('/') match {
-    case i if i <= 0 => "/"
-    case i => path.substring(0, i)
-  }
+  lazy val parentPath: String =
+    path.lastIndexOf('/') match {
+      case i if i <= 0 => "/"
+      case i => path.substring(0, i)
+    }
 
-  def childPath(child: String): String = path match {
-    case path if !path.endsWith("/") => path + "/" + child
-    case path => path + child
-  }
+  def childPath(child: String): String =
+    if (isRoot) {
+      if (child.startsWith("/")) child
+      else path + child
+    } else {
+      if (child.startsWith("/")) path + child
+      else path + "/" + child
+    }
+
+  def isRoot = "/" == path
 }
